76ad58438d306e7a9d1b34a0d6ddfe11ee34e859
Navid Ghahremani <ghahramani.navid@gmail.com>
2019-07-01 02:06:06 +0100

    removed thymeleaf added honcho to docker file change docker base image

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-logging")
     implementation("org.springframework.boot:spring-boot-starter-validation")
